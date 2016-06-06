c0f3e79aacca61c1c7033acdf8df3d2ccc87442d
liebesu <lichunjiang42@gmail.com>
2016-06-06 17:35:47 +0800

    update
    update

---
+++ fun_tv.py
@@ -0,0 +1,16 @@
+urllist=['http://www.ttmeiju.com/meiju/Game.of.Thrones.html']
+def check_disk():
+    pass
+def downloader_tv():
+    pass
+def check_url():
+    pass
+def downloader_zimu():
+    pass
+
+
+if __name__=="__main__":
+    check_disk()
+    check_url()
+    downloader_tv()
+    downloader_zimu()
