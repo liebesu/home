79162b0ebd0061be0ceeb85843cc7c56be26f2ac
liebesu <lichunjiang42@gmail.com>
2016-08-24 10:46:46 +0800

    update
    update

---
+++ home_crawler/lib/common/constants.py
@@ -0,0 +1,6 @@
+import os
+
+_current_dir = os.path.abspath(os.path.dirname(__file__))
+HOME_CRAWLER_ROOT = os.path.normpath(os.path.join(_current_dir, "..", ".."))
+
+
---
+++ poxiao.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+def db_insert():
+    
+if __name__=="__main__":
+    url='http://www.poxiao.com'
+    r=requests.get(url)
+    print r.status_code
