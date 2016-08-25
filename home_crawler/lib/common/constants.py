b9d3875910f221856dad2099a52b0ea4902b2452
liebesu <lichunjiang42@gmail.com>
2016-08-25 11:02:24 +0800

    update

--- home_crawler/lib/common/constants.py
+++ home_crawler/lib/common/constants.py
@@ -2,5 +2,5 @@ import os
 
 _current_dir = os.path.abspath(os.path.dirname(__file__))
 HOME_CRAWLER_ROOT = os.path.normpath(os.path.join(_current_dir, "..", ".."))
-
+THIRD_ROOT=os.path.normpath(os.path.join(_current_dir,"..","third"))
 
