2ea4272ba49b2d27f357a61c33961e230c9e4b77
liebesu <lichunjiang42@gmail.com>
2016-08-30 14:37:53 +0800

    update

--- home_crawler/ygdy8.py
+++ home_crawler/ygdy8.py
@@ -7,10 +7,10 @@ r=requests.get(url)
 soup=BeautifulSoup(r.content,"html5lib")
 lists=soup.find_all(href=re.compile("/html/gndy/dyzz/\d*"))
 for list in lists:
-    print list.get_text()
-    href=list.get('href')
-    href=urljoin(url, href)
-    if len(list.get_text())>8:
+    if len(list.get_text())>10:
+        print list.get_text()
+        href=list.get('href')
+        href=urljoin(url, href)        
         r=requests.get(href)
         new_soup=BeautifulSoup(r.content,"html5lib")
         download_url=new_soup.find(href=re.compile("thunder:*"))
