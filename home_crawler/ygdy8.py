225eb8182530cc0d522c5ef36a951f471d4c7052
liebesu <lichunjiang42@gmail.com>
2016-08-30 14:06:04 +0800

    error

--- home_crawler/ygdy8.py
+++ home_crawler/ygdy8.py
@@ -3,7 +3,7 @@ import re
 from bs4 import BeautifulSoup
 url='http://www.ygdy8.net/html/gndy/dyzz/index.html'
 r=requests.get(url)
-soup=BeautifulSoup(r.content,"html5.lib")
+soup=BeautifulSoup(r.content,"html5lib")
 lists=soup.find_all(href=re.compile("/html/gndy/dyzz/\d*"))
 for list in lists:
     print list.get_text()
