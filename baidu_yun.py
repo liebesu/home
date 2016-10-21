18513a861acf8debfb7b09af9cb2805423db99f3
liebesu <lichunjiang42@gmail.com>
2016-10-21 17:01:57 +0800

    home
    home

--- baidu_yun.py
+++ baidu_yun.py
@@ -1,10 +1,27 @@
 
 import requests
+from bs4 import BeautifulSoup
+from multiprocessing.pool import Pool
 
-url='http://pan.baidu.com/share/home?uk=3608297983#category/type=0'
-s=requests.session()
-s.headers.update({'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36'})
-r=requests.get(url)
-print r.encoding
-r.encoding = 'UTF-8'
-print r.text
+def get_name(id):
+    url='http://pan.baidu.com/share/home'
+    
+    headers={'User-Agent':'User-Agent:Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
+    payload={'uk':str(id)}
+    r=requests.get(url,params=payload,headers=headers)
+    print r.encoding
+    r.encoding = 'UTF-8'
+    soup=BeautifulSoup(r.content,'html5lib')
+    name=soup.find(class_='pic-frm-pic')
+    a=open('a.txt','a')
+    a.write(name.get('alt')+'/n')
+    a.close
+    
+
+if __name_=="__main__":
+    a=range(1,100000)
+    pool=Pool(processes=10)
+    pool.map(get_name, iterable)
+    pool.close()
+    pool.join()
+    
