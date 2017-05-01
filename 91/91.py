94886d3c5caa1bd0fac080825a0d8f07345aee3b
lichunjiang42@gmail.com <8589934593>
2017-05-01 23:27:56 +0800

    update

--- 91/91.py
+++ 91/91.py
@@ -3,16 +3,16 @@ import requests
 import json
 import re
 url='http://www.91porn.com/v.php'
-def get_tile_url()
+def get_tile_url():
     for i in range(1,3603):
         s=requests.session()
         headers={'Accept-Language':'zh-CN,zh;q=0.8,zh-TW;q=0.6',
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36'}
-        payloads={'next':'watch','page':i}
-        r=s.get(url,data=json.dumps(payloads),headers=headers)
+        payloads={'next':'watch','page':str(i)}
+        r=s.get(url,params=payloads,headers=headers)
+        print r.url
         soup=BeautifulSoup(r.content,"html5lib")
         alllist=soup.find_all(href=re.compile('http://www.91porn.com/view_video.php'))
-        print len(alllist)
         for a in alllist:
             if a.get('title'):
                 titletxt=a.get('title')
